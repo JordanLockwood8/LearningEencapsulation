85537b7f840a91d30572118a1ad58f46a5b71497
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-15 08:05:56 -0600

    Excersize 1

---
+++ EncapsulationEx1/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationEx1
+{
+    public class BankAccount
+    {
+        //field
+        private double balance = 0.0;
+
+
+        //Method
+        public void Deposit()
+        {
+            Console.WriteLine("enter deposit ammount in 00.00 format");
+            var deposit = double.Parse(Console.ReadLine());
+            balance += deposit;
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("enter withdraw amount in 00.00 format");
+            var withdraw = double.Parse(Console.ReadLine());
+            balance -= withdraw;
+
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine($"Your current balance is:{balance}");
+        }
+
+    }
+}
---
+++ EncapsulationEx1/Program.cs
@@ -0,0 +1,25 @@
+﻿using EncapsulationEx1;
+
+var bankAcct1 = new BankAccount();
+Console.WriteLine("Welcome to Your bank. What would you like to do?");
+Console.WriteLine("withdraw, deposit, or checkbalance");
+var input =Console.ReadLine();
+
+if (input == "withdraw")
+{
+    bankAcct1.Withdraw();
+    bankAcct1.GetBalance();
+}
+else if(input == "deposit")
+{
+    bankAcct1.Deposit();
+    bankAcct1.GetBalance();
+}
+else if( input == "checkbalance")
+{
+    bankAcct1.GetBalance();
+}
+else
+{
+    Console.WriteLine(" Improper input");
+}
